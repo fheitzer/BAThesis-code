a5da337e86e3229a59a9bd3fbd18fddb979c113b
Friedrich Heitzer <heizaas@gmx.de>
2022-04-08 13:58:22 +0200

    argument parsing

--- scripts/run.py
+++ scripts/run.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import argparse
 
 import sys; sys.path.insert(0, '..')
 import importlib
@@ -8,16 +9,34 @@ from lib import data, networks, training, utils
 
 
 if __name__ == '__main__':
-    train_ds_pre, train_ds_post, test_ds, train_generator, test_generator = data.load_data(rotation=30)
+    parser = argparse.ArgumentParser(description="Configure the run.")
+    
+    parser.add_argument('name', type=string,
+                        help='Name for the run')
+    parser.add_argument('rotation', type=int, 
+                        help='Amount of rotation in new data')
+    parser.add_argument('epochs', type=int,
+                        help='Amount of epochs per cycle per model')
+    parser.add_argument('batch_size', type=int,
+                        help='Batch size each epoch')
+    parser.add_argument('cycles', type=int,
+                        help='Amount of cycles')
+    parser.add_argument('data_per_cycle', type=int,
+                        help='Amount of data to look at before cycle')
+    
+    args = parser.parse_args()
+    
+    
+    train_ds_pre, train_ds_post, test_ds, train_generator, test_generator = data.load_data(rotation=args.rotation)
 
     num_classes = 10
     # Small model
     model1 = networks.NN([128, 128], num_classes)
     # Broad Model
     model2 = networks.NN([512], num_classes)
-    # Mixed Model
-    model3 = networks.CNN([(32, 3), (64, 5)])
     # cnn
+    model3 = networks.CNN([(32, 3), (64, 5)])
+    # cnn big
     model4 = networks.CNN([(32, 3), (64, 5), (128, 7)], num_classes)
     # cnn small
     model5 = networks.CNN([(32, 3), (64, 5)], num_classes)
@@ -29,12 +48,16 @@ if __name__ == '__main__':
     model3.load_weights('../models/CNN3264extra')
     model4.load_weights('../models/CNN3264128extra')
     model5.load_weights('../models/CNN3264extra')
-
+    
+    name = args.name
+    for arg in vars(args):
+        name += '_' + str(args[arg])
+    
     training.cycle(ensemble,
                    train_generator,
                    test_generator,
-                   epochs=3,
-                   batch_size=1,
-                   cycles=10,
-                   data_per_cycle=25000,
-		   name="SSHTEST_3_1_10_25000")
+                   epochs=args.epochs,
+                   batch_size=args.batch_size,
+                   cycles=args.cycles,
+                   data_per_cycle=args.data_per_cycle,
+                   name=name)
